16d26b22e0ccd1b156aadb427313653d75d79528
杜灼成 <duzhuocheng@duzhuochengdeMacBook-Pro.local>
2020-05-04 10:17:47 +0800

    update readme

--- README.md
+++ README.md
@@ -2,4 +2,34 @@
 
 a wechat robot which can tell you when the bus will come , base on '广州公交行讯通' & wechaty
 
+## example
+one wechat account to be robot, other wechat account can send message like '北京路', 
+and then you can get the response like this:
+
+
+公交车站：北京路<br>
+
+开往：昌岗路总站<br>
+5分钟后->10路
+ 
+开往：汾水小区总站<br>
+2分钟后->183路
+ 
+开往：芳村花园南门总站<br>
+9分钟后->1路
+ 
+开往：黄沙总站<br>
+2分钟后->219路
+ 
+开往：如意坊总站<br>
+3分钟后->3路
+ 
+开往：恒宝广场总站<br>
+3分钟后->541路
+ 
+开往：纸厂总站<br>
+3分钟后->544路
+ 
+开往：泮塘总站<br>
+10分钟后->66路
 
--- index.js
+++ index.js
@@ -5,39 +5,44 @@ const QrcodeTerminal = require('qrcode-terminal')
 
 const handler = require('./handler')
 
-const token = 'puppet_padplus_eee234cc1d3ebc3e'
+// const token = 'puppet_padplus_eee234cc1d3ebc3e'
 
-const puppet = new PuppetPadplus({
-    token,
-})
+// const puppet = new PuppetPadplus({
+//     token,
+// })
 
-const name = 'when-bus-come'
+// const name = 'when-bus-come'
 
-const bot = new Wechaty({
-    puppet,
-    name, // generate xxxx.memory-card.json and save login data for the next login
-})
+// const bot = new Wechaty({
+//     puppet,
+//     name, // generate xxxx.memory-card.json and save login data for the next login
+// })
 
-bot
-    .on('scan', (qrcode, status) => {
-        if (status === ScanStatus.Waiting) {
-            QrcodeTerminal.generate(qrcode, {
-                small: true
-            })
-        }
-    })
-    .on('message', async msg => {
-        const contact = msg.from()
-        const busStation = msg.text().trim()
-        try {
-            const res = await handler(busStation)
-            botMsger(contact, res)
-        } catch (eMsg) {
-            botMsger(contact, eMsg)
-        }
-    })
-    .start()
+// bot
+//     .on('scan', (qrcode, status) => {
+//         if (status === ScanStatus.Waiting) {
+//             QrcodeTerminal.generate(qrcode, {
+//                 small: true
+//             })
+//         }
+//     })
+//     .on('message', async msg => {
+//         const contact = msg.from()
+//         const busStation = msg.text().trim()
+//         try {
+//             const res = await handler(busStation)
+//             botMsger(contact, res)
+//         } catch (eMsg) {
+//             botMsger(contact, eMsg)
+//         }
+//     })
+//     .start()
 
-function botMsger(contact, text) {
-    contact.say(text)
+// function botMsger(contact, text) {
+//     contact.say(text)
+// }
+async function test(){
+    const res = await handler('北京路')
+    console.log({res})
 }
+test()
